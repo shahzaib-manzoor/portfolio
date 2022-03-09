de701e5366644a20b9f1199f96df8f0a47d7a68e
shahzaib-manzoor <shahzi113awan@gmail.com>
2022-03-09 22:54:43 +0500

    utils

---
+++ utils/data/educations.js
@@ -0,0 +1,20 @@
+export const educations = [
+  {
+    id: 1,
+    title: "Bachelor Degree",
+    duration: "2016 - 2020",
+    institution: "Punjab University College of Information Technology, Lahore",
+  },
+  {
+    id: 2,
+    title: "FS.C Pre-Engineering",
+    duration: "2013-2015",
+    institution: "Punjab Group of Colleges, Faisalabad",
+  },
+  {
+    id: 3,
+    title: "Govt A.V Modern High School, Faisalabad",
+    duration: "2011 - 2013",
+    institution: "Baitus Saif Islamia Madrasah",
+  }
+]
