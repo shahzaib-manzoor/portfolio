ea53b11ec67d5f1eb0693e8e1776a8a2f24a78e2
shahzaib-manzoor <shahzi113awan@gmail.com>
2022-03-09 22:54:38 +0500

    utils

---
+++ utils/data/contactsData.js
@@ -0,0 +1,11 @@
+export const contactsData = {
+    email: 'shahzi113awan@gmail.com',
+    phone: '+923486933821',
+    address: 'Lahore, Pakistan',
+    github: 'https://github.com/shahzaib-manzoor',
+    facebook: 'https://www.facebook.com/shahzi113awan',
+    linkedIn: 'https://www.linkedin.com/in/shahzaib-manzoor-045b72120/',
+    twitter: 'https://twitter.com/shahzi113awan',
+    stackOverflow: 'https://stackoverflow.com/users/7108419/shahzaib-manzoor',
+    devUsername: "shahzi113awan",
+}
