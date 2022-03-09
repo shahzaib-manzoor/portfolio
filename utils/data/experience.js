8d12f664bffc2a9b7938d58f268d09cd1d72c38b
shahzaib-manzoor <shahzi113awan@gmail.com>
2022-03-09 22:54:48 +0500

    utils

---
+++ utils/data/experience.js
@@ -0,0 +1,20 @@
+export const experiences = [
+  {
+    id: 1,
+    title: 'Software Engineer',
+    company: "Mayfair Technologies partners",
+    duration: "(Feb 2023 - Present)"
+  },
+  {
+    id: 2,
+    title: "FullStack Developer",
+    company: "RedFireAi pvt ltd",
+    duration: "(Feb 2022 - Feb 2023)"
+  },
+  {
+    id: 3,
+    title: "Associate Software Engineer",
+    company: "Maqware Solutions",
+    duration: "(May 2019 - Feb 2022)"
+  }
+]
