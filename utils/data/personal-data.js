1abe45248c3546ddb1e5d73eca19c2fd04e53de5
shahzaib-manzoor <shahzi113awan@gmail.com>
2022-03-09 22:54:52 +0500

    utils

---
+++ utils/data/personal-data.js
@@ -0,0 +1,17 @@
+export const personalData = {
+  name: "Shahzaib Manzoor",
+  profile: '/profile.png',
+  designation: "Software Developer",
+  description: "My name is Shahzaib Manzoor. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests. with over 4 years of professional experience in web development, I have worked on various projects and have gained a lot of experience in the field.",
+  email: 'shahzi113awan@gmail.com',
+  phone: '+923486933821',
+  address: 'Lahore, Pakistan',  
+  github: 'https://github.com/shahzaib-manzoor',
+  facebook: 'https://www.facebook.com/shahzi113awan',
+  linkedIn: 'https://www.linkedin.com/in/shahzaib-manzoor-045b72120/',
+  twitter: 'https://twitter.com/shahzi113awan',
+  stackOverflow: 'https://stackoverflow.com/users/7108419/shahzaib-manzoor',
+  leetcode: "",
+  devUsername: "shahzi113awan",
+  resume: "https://drive.google.com/file/d/1-ZpZmPYwisUrRbqopIGxu0Orq5xVnH8S/view?usp=drive_link"
+}
